fedf76fe7ede2714f65cb10ee1807ed9e4290d46
allen hdz <allen.hernandez200@gmail.com>
2023-11-18 22:13:09 -0600

    trophy progress

--- marioTracker/static/marioTracker/js/fireworks.js
+++ marioTracker/static/marioTracker/js/fireworks.js
@@ -22,7 +22,7 @@ function resizeCanvas(){
 function updateWorld(){
     update();
     paint();
-    window.requestAnimationFrame();
+    window.requestAnimationFrame(updateWorld);
 }
 
 function update(){
@@ -40,7 +40,8 @@ function update(){
 
 function paint() {
     ctx.globalCompositeOperation = 'source-over';
-    ctx.fillStyle = "rgba(0, 0, 0, 0.2)";
+    ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
+    ctx.fillRect(0, 0, width ,height);
     ctx.globalCompositeOperation = 'lighter';
     for(var i = 0; i < particles.length; i++){
         particles[i].draw(ctx);
@@ -49,7 +50,60 @@ function paint() {
 
 function createFirework() {
     xPoint = Math.random()*(width - 200) + 100;
-    yPoint = Math.random()*(width - 200) + 100;
+    yPoint = Math.random()*(height/3 - 200) + 100;
     var nFire = Math.random() * 50 + 100;
-    var c = "rgb("+(~~(Math.random()*200 + )
+    var c = "rgb("+(~~(Math.random()*200 + 55)) + ", " +
+                + (~~(Math.random()*200 + 55)) + ", " +
+                + (~~(Math.random()*200 + 55)) + ") ";
+    
+    for(var i = 0; i < nFire; i++){
+        var particle = new Particle();
+        particle.color = c;
+        var vy = Math.sqrt(25-particle.vx*particle.vx);
+        if(Math.abs(particle.vy) > vy){
+            particle.vy = particle.vy > 0 ? vy : -vy;
+        }
+        particles.push(particle);
+    }
+}
+
+function Particle(){
+    this.w = this.h = Math.random()*4+1;
+
+    this.x = xPoint-this.w/2;
+    this.y = yPoint-this.h;
+
+    this.vx = (Math.random()-0.5) * 10;
+    this.vy = (Math.random()-0.5) * 10;
+
+    this.alpha = Math.random()*0.5 + 0.5;
+    this.color;
+}
+
+Particle.prototype = {
+    gravity: 0.05,
+    move: function () {
+        this.x += this.vx;
+        this.vy += this.gravity;
+        this.y += this.vy;
+        this.alpha -= 0.01;
+        if(this.x <= -this.w || this.x >= screen.width || 
+            this.y >= screen.height || this.alpha <= 0){
+                return false;
+            }
+        return true;
+    },
+    draw: function (c) {
+        c.save();
+        c.beginPath();
+
+        c.translate(this.x+this.w/2, this.y+this.h/2);
+        c.arc(0, 0, this.w, 0, Math.PI * 2);
+        c.fillStyle = this.color;
+        c.globalAlpha = this.alpha;
+
+        c.closePath();
+        c.fill();
+        c.restore();
+    }
 }
