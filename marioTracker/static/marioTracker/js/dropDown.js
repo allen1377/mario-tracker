d0bc6dd3a8cb098ae8ffab659937db2a2a3733f3
allen1377 <allen.hernandez200@gmail.com>
2024-09-23 22:48:25 -0500

    added search bar and some select abilities, in progress

--- marioTracker/static/marioTracker/js/dropDown.js
+++ marioTracker/static/marioTracker/js/dropDown.js
@@ -1,17 +1,13 @@
 $(document).ready(function (){
     $('.dropdown').on('click', function(){
-        console.log("Passes dropdown function--------------")
         if (!($('.dropdown-menu').hasClass('dropDownHidden') || $('.dropdown-menu').hasClass('dropDownFlex'))){
             $('.dropdown-menu').addClass('dropDownFlex');
-            console.log("First If")
         }
         else if ($('.dropdown-menu').hasClass('dropDownHidden')){
             $('.dropdown-menu').removeClass('dropDownHidden').addClass('dropDownFlex');
-            console.log("Second If")
         }
         else if ($('.dropdown-menu').hasClass('dropDownFlex')){
             $('.dropdown-menu').removeClass('dropDownFlex').addClass('dropDownHidden');
-            console.log("Else: ", $(this));
         }
     });
 
--- marioTracker/static/marioTracker/js/searchBar.js
+++ marioTracker/static/marioTracker/js/searchBar.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
     $('#searchBar').on('keyup', function() {
         var searchTerm = $(this).val().toLowerCase();
-
+        
         //console.log("Hello");
         $('.dropdown-menu li').each(function() {
-            var firstName = $(this).data('firstName');
-            var lastName = $(this).data('lastName');
-
-            console.log("Search Term: ", searchTerm, ", firstName lastName: ", firstName, " ", lastName);
+            var firstName = $(this).data('firstname').toLowerCase();
+            var lastName = $(this).data('lastname').toLowerCase();
+            
             if (firstName.includes(searchTerm) || lastName.includes(searchTerm)) {
                 $(this).show();
             }
@@ -15,5 +14,23 @@ $(document).ready(function() {
                 $(this).hide();
             }
         });
+    });
+
+    $('.dropdown-menu li').on('click', function(){
+        var firstName = $(this).data('firstname');
+        var lastName = $(this).data('lastname');
+        var fullName = firstName + ' ' + lastName;
+
+        if ($('#selectedPlayers').find(`[data-player="${fullName}"]`).length === 0){
+            $('#selectedPlayers').append(`
+                <div class="selectedPlayer" data-player="${fullName}">
+                    "${fullName}" <span class="removePlayer">x</span>
+                </div>
+            `);
+        }
+    });
+
+    $(document).on('click', '.removePlayer', function() {
+        $(this).parent('.selectedPlayer').remove();
     });
 });
